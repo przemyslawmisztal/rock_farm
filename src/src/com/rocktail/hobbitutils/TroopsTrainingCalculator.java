199949d0981ab37d757db029c3bd3bdf4ef6afa4
rocktail <oldie.fiction+github@gmail.com>
2014-04-27 09:38:55 +0200

    added list of units (parameters, name, cost etc) to calculator

--- src/src/com/rocktail/hobbitutils/TroopsTrainingCalculator.java
+++ src/src/com/rocktail/hobbitutils/TroopsTrainingCalculator.java
@@ -1,6 +1,9 @@
 package com.rocktail.hobbitutils;
 
+import java.util.List;
+
 import com.rocktail.hobbitutilst.models.PlayerResources;
+import com.rocktail.hobbitutilst.models.UnitModel;
 
 /**
  * Handles calculations that provide answer what troops to train to spend most of the resources  
@@ -9,13 +12,15 @@ import com.rocktail.hobbitutilst.models.PlayerResources;
  */
 public class TroopsTrainingCalculator {
 	private PlayerResources _playerResources;
-	
+	private List<UnitModel> _units;
 	/**
 	 * Creates new instance of {@link TroopsTrainingCalculator}
 	 * @param playerResources
 	 */
-	public TroopsTrainingCalculator(PlayerResources playerResources) {
+	public TroopsTrainingCalculator(PlayerResources playerResources,
+			List<UnitModel> unitsToProduce) {
 		this.setPlayerResources(playerResources);
+		this.setUnits(unitsToProduce);
 	}
 
 	/**
@@ -32,6 +37,22 @@ public class TroopsTrainingCalculator {
 	 */
 	private void setPlayerResources(PlayerResources _playerResources) {
 		this._playerResources = _playerResources;
+	}
+
+	/**
+	 * Gets list of units we want to produce
+	 * @return
+	 */
+	public List<UnitModel> getUnits() {
+		return _units;
+	}
+
+	/**
+	 * Sets list of units that we want to produce
+	 * @param _units
+	 */
+	private void setUnits(List<UnitModel> _units) {
+		this._units = _units;
 	}
 
 }
--- tests/HobbitUtilsTests/src/com/rocktail/hobbitutils/test/TroopsTrainingCalculatorTests.java
+++ tests/HobbitUtilsTests/src/com/rocktail/hobbitutils/test/TroopsTrainingCalculatorTests.java
@@ -1,10 +1,15 @@
 package com.rocktail.hobbitutils.test;
 
+import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import android.test.AndroidTestCase;
 
 import com.rocktail.hobbitutils.TroopsTrainingCalculator;
 import com.rocktail.hobbitutilst.models.PlayerResources;
-import static org.mockito.Mockito.*;
+import com.rocktail.hobbitutilst.models.UnitModel;
 
 /**
  * Class contains unit tests for TroopsTrainingCalculator
@@ -19,7 +24,9 @@ public class TroopsTrainingCalculatorTests extends AndroidTestCase {
 	public void testCanCreateTroopsTrainingCalculator() {
 		//arrange
 		//act
-		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(GetPlayerResourcesMock());
+		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(
+				GetPlayerResourcesMock(),
+				GetUnitModels());
 		
 		//assert
 		assertNotNull(sut);
@@ -28,18 +35,24 @@ public class TroopsTrainingCalculatorTests extends AndroidTestCase {
 	private PlayerResources GetPlayerResourcesMock() {
 		return mock(PlayerResources.class);
 	}
+	
+	private List<UnitModel> GetUnitModels() {
+		List<UnitModel> unitModels = new ArrayList<UnitModel>();
+		
+		return unitModels;
+	}
 	/**
 	 * Testing if constructor properly assigns {@link PlayerResources}
 	 */
 	public void testPlayerResourcesAreSameWhenPassedThroughConstructor() {
 		//arrange
 		PlayerResources playerResources = GetPlayerResourcesMock();
-		
+		List<UnitModel> unitModels = GetUnitModels();
 		//act
-		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(playerResources);
+		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(playerResources, unitModels);
 		
 		//assert
 		assertSame(playerResources, sut.getPlayerResources());
-		
+		assertSame(playerResources, sut.getUnits());
 	}
 }
