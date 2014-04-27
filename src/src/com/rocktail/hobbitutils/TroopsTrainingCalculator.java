4eec9b15dc5cab96c50382e6157ecd162ebd6d47
rocktail <oldie.fiction+github@gmail.com>
2014-04-27 09:24:01 +0200

    added PlayerResources to TroopsTrainingCalculator

--- src/src/com/rocktail/hobbitutils/TroopsTrainingCalculator.java
+++ src/src/com/rocktail/hobbitutils/TroopsTrainingCalculator.java
@@ -1,10 +1,37 @@
 package com.rocktail.hobbitutils;
 
+import com.rocktail.hobbitutilst.models.PlayerResources;
+
 /**
  * Handles calculations that provide answer what troops to train to spend most of the resources  
  * @author rocktail
  *
  */
 public class TroopsTrainingCalculator {
+	private PlayerResources _playerResources;
+	
+	/**
+	 * Creates new instance of {@link TroopsTrainingCalculator}
+	 * @param playerResources
+	 */
+	public TroopsTrainingCalculator(PlayerResources playerResources) {
+		this.setPlayerResources(playerResources);
+	}
+
+	/**
+	 * Gets {@link PlayerResources}
+	 * @return
+	 */
+	public PlayerResources getPlayerResources() {
+		return this._playerResources;
+	}
+
+	/**
+	 * Sets {@link PlayerResources}
+	 * @param _playerResources
+	 */
+	private void setPlayerResources(PlayerResources _playerResources) {
+		this._playerResources = _playerResources;
+	}
 
 }
--- tests/HobbitUtilsTests/src/com/rocktail/hobbitutils/test/TroopsTrainingCalculatorTests.java
+++ tests/HobbitUtilsTests/src/com/rocktail/hobbitutils/test/TroopsTrainingCalculatorTests.java
@@ -2,11 +2,44 @@ package com.rocktail.hobbitutils.test;
 
 import android.test.AndroidTestCase;
 
+import com.rocktail.hobbitutils.TroopsTrainingCalculator;
+import com.rocktail.hobbitutilst.models.PlayerResources;
+import static org.mockito.Mockito.*;
+
 /**
  * Class contains unit tests for TroopsTrainingCalculator
  * @author rocktail
  *
  */
 public class TroopsTrainingCalculatorTests extends AndroidTestCase {
-
+	
+	/**
+	 * Basic constructor test
+	 */
+	public void testCanCreateTroopsTrainingCalculator() {
+		//arrange
+		//act
+		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(GetPlayerResourcesMock());
+		
+		//assert
+		assertNotNull(sut);
+	}
+	
+	private PlayerResources GetPlayerResourcesMock() {
+		return mock(PlayerResources.class);
+	}
+	/**
+	 * Testing if constructor properly assigns {@link PlayerResources}
+	 */
+	public void testPlayerResourcesAreSameWhenPassedThroughConstructor() {
+		//arrange
+		PlayerResources playerResources = GetPlayerResourcesMock();
+		
+		//act
+		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(playerResources);
+		
+		//assert
+		assertSame(playerResources, sut.getPlayerResources());
+		
+	}
 }
