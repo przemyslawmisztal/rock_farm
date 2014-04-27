f237c7e66e203dcad492924ccaf2a7988139675a
rocktail <oldie.fiction+github@gmail.com>
2014-04-27 09:12:59 +0200

    added comments to UnitModel

--- src/src/com/rocktail/hobbitutilst/models/UnitModel.java
+++ src/src/com/rocktail/hobbitutilst/models/UnitModel.java
@@ -19,6 +19,7 @@ public class UnitModel extends Observable {
 	private int _tier;
 	private UnitType _unitType;
 	private String _INVALID_UNIT_TYPE_MSG = "No or invalid unit type provided.";
+	
 	/**
 	 * Using constructor is the only way to assign values to instance of this class
 	 * @param name
@@ -47,39 +48,75 @@ public class UnitModel extends Observable {
 		this.setUnitType(unitType);
 	}
 	
+	/**
+	 * Get name of the unit
+	 * @return
+	 */
 	public String getName() {
 		return _name;
 	}
 
+	/**
+	 * Get unit cost in wood
+	 * @return
+	 */
 	public long getCostInWood() {
 		return _costInWood;
 	}
 
+	/**
+	 * Get unit cost in stone
+	 * @return
+	 */
 	public long getCostInStone() {
 		return _costInStone;
 	}
 
+	/**
+	 * Get unit cost in ore
+	 * @return
+	 */
 	public long getCostInOre() {
 		return _costInOre;
 	}
 
+	/**
+	 * Get unit might
+	 * @return
+	 */
 	public int getMight() {
 		return _might;
 	}
 
+	/**
+	 * Get unit tier
+	 * @return
+	 */
 	public int getTier() {
 		return _tier;
 	}
 
+	/**
+	 * Get unit cost in food
+	 * @return
+	 */
 	public long getCostInFood() {
 		return _costInFood;
 	}
 
+	/**
+	 * Get unit type
+	 * @return
+	 */
 	public UnitType getUnitType() {
 		return _unitType;
 	}
 
-	public void setUnitType(UnitType unitType) {
+	/**
+	 * Set unit type.
+	 * @param unitType
+	 */
+	private void setUnitType(UnitType unitType) {
 		if (unitType == null || unitType == UnitType.Invalid) {
 			throw new IllegalArgumentException(this._INVALID_UNIT_TYPE_MSG);
 		}
