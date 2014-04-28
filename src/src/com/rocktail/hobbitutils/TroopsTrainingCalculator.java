8b35b9a8e77da500336a29294f67ff9a6f5d7923
rocktail <oldie.fiction+github@gmail.com>
2014-04-28 20:04:20 +0200

    added configuration assets for TroopsTrainingCalculator

--- src/src/com/rocktail/hobbitutils/TroopsTrainingCalculator.java
+++ src/src/com/rocktail/hobbitutils/TroopsTrainingCalculator.java
@@ -43,16 +43,16 @@ public class TroopsTrainingCalculator {
 		
         try {       	
             double[][] A = PrepareMatrixForSimplex_A(
-            		this._tier1Foot,
-            		this._tier1Mounted,
-            		this._tier1Ranged);
+            		this.getTier1Foot(),
+            		this.getTier1Mounted(),
+            		this.getTier1Ranged());
             
-            double[] c = PrepareMatrixForSimplex_C(this._tier1Foot, this._tier1Mounted, this._tier1Ranged);
+            double[] c = PrepareMatrixForSimplex_C(this.getTier1Foot(), this.getTier1Mounted(), this.getTier1Ranged());
             double[] b = PrepareMatrixForSimplex_B();
                 
             Simplex lp = new Simplex(A, b, c);
 
-            //we are interested only in primal values (x)
+            //we are interested only in primely values (x)
             double[] x = lp.primal();
             
             //first three values are the ones that we are searching for
@@ -71,22 +71,34 @@ public class TroopsTrainingCalculator {
 	}
 
 	private double[] PrepareMatrixForSimplex_B() {
-		double[] b = { this._playerResources.getFood(), this._playerResources.getWood(), this._playerResources.getStone(), this._playerResources.getOre() };
+		double[] b = { 
+				this._playerResources.getFood(),
+				this._playerResources.getWood(),
+				this._playerResources.getStone(),
+				this._playerResources.getOre() };
 		
 		return b;
 	}
 	private double[] PrepareMatrixForSimplex_C(HobbitUnitDefinitionVO t1_1, HobbitUnitDefinitionVO t1_2, HobbitUnitDefinitionVO t1_3) {
-		double[] c = { t1_1.getMight(), t1_2.getMight(), t1_2.getMight(), 0, 0, 0, 0 };
+		double[] c = { 
+				t1_1.getMight(),
+				t1_2.getMight(),
+				t1_2.getMight(),
+				0,
+				0,
+				0,
+				0 };
 		
 		return c;
 	}
 	
+	
 	private double[][] PrepareMatrixForSimplex_A(HobbitUnitDefinitionVO t1_1, HobbitUnitDefinitionVO t1_2, HobbitUnitDefinitionVO t1_3) {
 		double[][] A =  {
-            { t1_1.getCostInFood(), t1_2.getCostInFood(), t1_3.getCostInFood(), 1, 0, 0, 0},
-            { t1_1.getCostInWood(), t1_2.getCostInWood(), t1_3.getCostInWood(),  0, 1, 0, 0},
-            { t1_1.getCostInStone(),  t1_2.getCostInStone(), t1_2.getCostInStone(), 0, 0, 1, 0},
-            { t1_1.getCostInOre(), t1_2.getCostInOre(),  t1_2.getCostInOre(), 0, 0, 0, 1},
+            { t1_1.getCostInFood(), 	t1_2.getCostInFood(),	t1_3.getCostInFood(),	1, 0, 0, 0},
+            { t1_1.getCostInWood(),		t1_2.getCostInWood(),	t1_3.getCostInWood(),  	0, 1, 0, 0},
+            { t1_1.getCostInStone(),	t1_2.getCostInStone(),	t1_2.getCostInStone(), 	0, 0, 1, 0},
+            { t1_1.getCostInOre(), 		t1_2.getCostInOre(),	t1_2.getCostInOre(), 	0, 0, 0, 1},
         };
 		
 		return A;
@@ -126,16 +138,52 @@ public class TroopsTrainingCalculator {
 		this._playerResources = _playerResources;
 	}
 
-	private void setTier1Foot(HobbitUnitDefinitionVO _tier1foot) {
-		this._tier1Foot = _tier1foot;
+	/**
+	 * Sets properties of tier 1 foot units
+	 * @param tier1foot
+	 */
+	private void setTier1Foot(HobbitUnitDefinitionVO tier1foot) {
+		this._tier1Foot = tier1foot;
 	}
 
-	public void setTier1Ranged(HobbitUnitDefinitionVO _tier1Ranged) {
-		this._tier1Ranged = _tier1Ranged;
+	/**
+	 * Sets properties of tier 1 ranged units
+	 * @param tier1Ranged
+	 */
+	private void setTier1Ranged(HobbitUnitDefinitionVO tier1Ranged) {
+		this._tier1Ranged = tier1Ranged;
 	}
 
-	public void setTier1Mounted(HobbitUnitDefinitionVO _tier1Mounted) {
-		this._tier1Mounted = _tier1Mounted;
+	/**
+	 * Sets properties of tier 1 mounted units
+	 * @param tier1Mounted
+	 */
+	private void setTier1Mounted(HobbitUnitDefinitionVO tier1Mounted) {
+		this._tier1Mounted = tier1Mounted;
+	}
+
+	/**
+	 * Gets properties of tier 1 foot units
+	 * @return
+	 */
+	public HobbitUnitDefinitionVO getTier1Foot() {
+		return _tier1Foot;
+	}
+
+	/**
+	 * Gets properties of tier 1 ranged units
+	 * @return
+	 */
+	public HobbitUnitDefinitionVO getTier1Ranged() {
+		return _tier1Ranged;
+	}
+
+	/**
+	 * Gets properties of tier 1 mounted units
+	 * @return
+	 */
+	public HobbitUnitDefinitionVO getTier1Mounted() {
+		return _tier1Mounted;
 	}
 
 }
--- tests/HobbitUtilsTests/src/com/rocktail/hobbitutils/test/TroopsTrainingCalculatorTests.java
+++ tests/HobbitUtilsTests/src/com/rocktail/hobbitutils/test/TroopsTrainingCalculatorTests.java
@@ -3,6 +3,9 @@ package com.rocktail.hobbitutils.test;
 import android.test.AndroidTestCase;
 
 import com.rocktail.hobbitutils.TroopsTrainingCalculator;
+import com.rocktail.hobbitutils.vos.HobbitConfigurationVO;
+import com.rocktail.hobbitutils.vos.HobbitUnitDefinitionVO;
+import com.rocktail.hobbitutils.vos.UnitType;
 import com.rocktail.hobbitutilst.models.PlayerResources;
 
 /**
@@ -11,37 +14,63 @@ import com.rocktail.hobbitutilst.models.PlayerResources;
  *
  */
 public class TroopsTrainingCalculatorTests extends AndroidTestCase {
-	
+	private int _SAMPLE_AMOUNT = 10000;
 	/**
 	 * Basic constructor test
 	 */
 	public void testCanCreateTroopsTrainingCalculator() {
 		//arrange
 		//act
-		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(getPlayerResources(), null);
+		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(getPlayerResources(), getHobbitConfiguration());
 		
 		//assert
 		assertNotNull(sut);
 	}
 	
 	private PlayerResources getPlayerResources() {
-		PlayerResources playerResources = new PlayerResources(10000, 10000, 10000, 10000);
+		//TODO: mock it
+		PlayerResources playerResources = new PlayerResources(
+				this._SAMPLE_AMOUNT,
+				this._SAMPLE_AMOUNT, 
+				this._SAMPLE_AMOUNT,
+				this._SAMPLE_AMOUNT);
 		
 		return playerResources;
 	}
 	
+	private HobbitConfigurationVO getHobbitConfiguration() {
+		//TODO: mock it
+		int costFood = this._SAMPLE_AMOUNT;
+		int costWood = this._SAMPLE_AMOUNT;
+		int costOre = this._SAMPLE_AMOUNT;
+		int costStone = this._SAMPLE_AMOUNT;
+		int might = this._SAMPLE_AMOUNT;
+		String name = "testUnit";
+		int tier = this._SAMPLE_AMOUNT;
+		UnitType unitType = UnitType.Foot;
+		
+		HobbitUnitDefinitionVO tier1Foot = new HobbitUnitDefinitionVO(name, costFood, costWood, costStone, costOre, might, tier, unitType);
+		HobbitUnitDefinitionVO tier1Mounted = new HobbitUnitDefinitionVO(name, costFood, costWood, costStone, costOre, might, tier, unitType);
+		HobbitUnitDefinitionVO tier1Ranged = new HobbitUnitDefinitionVO(name, costFood, costWood, costStone, costOre, might, tier, unitType);
+		
+		return new HobbitConfigurationVO(tier1Foot, tier1Mounted, tier1Ranged);
+		
+		
+	}
 	/**
 	 * Testing if constructor properly assigns {@link PlayerResources}
 	 */
 	public void testPlayerResourcesAreSameWhenPassedThroughConstructor() {
 		//arrange
 		PlayerResources playerResources = getPlayerResources();
-
+		HobbitConfigurationVO config = getHobbitConfiguration();
 		//act
-		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(playerResources, null);
+		TroopsTrainingCalculator sut = new TroopsTrainingCalculator(playerResources, config);
 		
 		//assert
 		assertSame(playerResources, sut.getPlayerResources());
-		//TODO: assert configuration
+		assertSame(config.getTier1FootUnit(), sut.getTier1Foot());
+		assertSame(config.getTier1MountedUnit(), sut.getTier1Mounted());
+		assertSame(config.getTier1RangedUnit(), sut.getTier1Ranged());
 	}
 }
