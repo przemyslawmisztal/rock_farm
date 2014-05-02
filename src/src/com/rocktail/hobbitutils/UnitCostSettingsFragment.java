ed411c751ca038a88c7cf786d35db1b91312ee3e
rocktail <oldie.fiction+github@gmail.com>
2014-05-02 10:47:22 +0200

    added value initialization when creating settings activity

--- src/src/com/rocktail/hobbitutils/UnitCostSettingsFragment.java
+++ src/src/com/rocktail/hobbitutils/UnitCostSettingsFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 
 public class UnitCostSettingsFragment extends PreferenceFragment 
@@ -15,7 +16,29 @@ public class UnitCostSettingsFragment extends PreferenceFragment
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+        
+        //we want to load current values initially
+		for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+        	initSummary(getPreferenceScreen().getPreference(i));
+		}
     }
+	
+	/**
+	 * Assign setting values to their respective summary fields 
+	 */
+	private void initSummary(Preference preference) {
+		if (preference instanceof PreferenceCategory) {
+            PreferenceCategory pCategory = (PreferenceCategory) preference;
+            
+            for (int i = 0; i < pCategory.getPreferenceCount(); i++) {
+                initSummary(pCategory.getPreference(i));
+            }
+        } else {
+        	if (preference instanceof EditTextPreference) {
+    	    	updateSummary((EditTextPreference) preference);
+        	}
+        }
+	}
 	
 	/**
 	 * Displaying current setting value in summary field
@@ -24,9 +47,12 @@ public class UnitCostSettingsFragment extends PreferenceFragment
 	    Preference pref = findPreference(key);
 
 	    if (pref instanceof EditTextPreference) {
-	    	EditTextPreference listPref = (EditTextPreference) pref;
-	        pref.setSummary(listPref.getText());
+	    	updateSummary((EditTextPreference) pref);
 	    }
+	}
+	
+	private void updateSummary(EditTextPreference editTextPreference) {
+		editTextPreference.setSummary(editTextPreference.getText());
 	}
 	
 	@Override
