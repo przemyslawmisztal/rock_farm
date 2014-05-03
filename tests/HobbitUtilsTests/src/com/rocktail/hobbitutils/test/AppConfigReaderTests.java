037cca268a1e6480288770c90ea50ea84b878c85
rocktail <oldie.fiction+github@gmail.com>
2014-05-03 10:52:09 +0200

    added config reader tests

--- tests/HobbitUtilsTests/src/com/rocktail/hobbitutils/test/AppConfigReaderTests.java
+++ tests/HobbitUtilsTests/src/com/rocktail/hobbitutils/test/AppConfigReaderTests.java
@@ -1,10 +1,29 @@
 package com.rocktail.hobbitutils.test;
 
+import junit.framework.Assert;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.test.mock.MockContext;
+import android.test.AndroidTestCase;
+
+import com.rocktail.hobbitutils.utils.AppConfigReader;
+import com.rocktail.hobbitutils.vos.HobbitConfigurationVO;
+
 /**
  * Class containing tests for configuration reader 
  * @author rocktail
  *
  */
-public class AppConfigReaderTests {
-
+public class AppConfigReaderTests extends AndroidTestCase {
+	/**
+	 * Basic constructor test
+	 */
+	public void testCanCreateReader() {
+		//arrange
+		//act
+		AppConfigReader sut = new AppConfigReader();
+		
+		//assert
+		assertNotNull(sut);
+	}
 }
