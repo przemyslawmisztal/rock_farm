8d6f2c032b661e4de450900412496ad25933d6e4
rocktail <oldie.fiction+github@gmail.com>
2014-05-04 10:45:03 +0200

    added enum ResourceType with all types of resources available in game

--- src/src/com/rocktail/hobbitutils/activities/PlayerResourceView.java
+++ src/src/com/rocktail/hobbitutils/activities/PlayerResourceView.java
@@ -1,18 +1,45 @@
 package com.rocktail.hobbitutils.activities;
 
 import android.content.Context;
+import android.widget.EditText;
 import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Compound view for player resource input 
  * @author rocktail
  *
  */
-public class PlayerResourceView extends GridLayout {
-
+public class PlayerResourceView extends GridLayout
+	implements IPlayerResourceView {
+	
+	private TextView _tileTitleTextView;
+	private EditText _resourceAmountEditText;
+	private ImageView _tileIconImageView;
+	
 	public PlayerResourceView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setAmount(long resourceAmount) {
+		this._resourceAmountEditText.setText(String.valueOf(resourceAmount));
+		
+	}
+
+	@Override
+	public void setResourceName(String resourceName) {
+		this._tileTitleTextView.setText(resourceName);
+		
+	}
+
+	@Override
+	public void setResourceIcon(String iconName) {
+		int resId = getResources().getIdentifier(iconName, "drawable", "com.rocktail.hobbitutils");
+		this._tileIconImageView.setImageResource(resId);
+		
 	}
 
 }
---
+++ src/src/com/rocktail/hobbitutils/vos/ResourceType.java
@@ -0,0 +1,10 @@
+package com.rocktail.hobbitutils.vos;
+
+/**
+ * Enumeration for resource types
+ * @author rocktail
+ *
+ */
+public enum ResourceType {
+	Invalid, Food, Wood, Stone, Ore
+}
