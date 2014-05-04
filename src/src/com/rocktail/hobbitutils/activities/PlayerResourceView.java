78a6ec482ad281b49ef818b75727ac88cacb5e96
rocktail <oldie.fiction+github@gmail.com>
2014-05-04 10:03:36 +0200

    added dark background_dark.png, created PlayerResourceView to separate player input UI and logic components

---
+++ src/src/com/rocktail/hobbitutils/activities/PlayerResourceView.java
@@ -0,0 +1,18 @@
+package com.rocktail.hobbitutils.activities;
+
+import android.content.Context;
+import android.widget.GridLayout;
+
+/**
+ * Compound view for player resource input 
+ * @author rocktail
+ *
+ */
+public class PlayerResourceView extends GridLayout {
+
+	public PlayerResourceView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
